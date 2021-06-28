c89d8e5f635e127fd00f643c5dfc8c303a60218f
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-06-28 12:40:20 -0400

    validation input Type

--- src/Components/Restricted/Classroom.Create.tsx
+++ src/Components/Restricted/Classroom.Create.tsx
@@ -14,23 +14,28 @@ import {
 } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { cities, City } from '../../Assets/cities';
-import { getTerritoryNames, TerritoryType } from '../../Functions/GetTerritoryList';
+import { getTerritoryNames, LandType } from '../../Functions/GetTerritoryList';
+import { IClassroom } from '../../Models/Classroom.interface';
+import { IPlace } from '../../Models/Place.interface';
 
 export const ClassroomCreate = () => {
-  const [selectedDate, setSelectedDate] = React.useState<Date>(new Date());
-
   //Land type and land list
-  const [landType, setLandType] = React.useState<TerritoryType>(TerritoryType.region);
+  const [landType, setLandType] = React.useState<LandType>(LandType.region);
   const [landList, setLandList] = React.useState<string[]>([]);
+  const [selectDate, setSelectDate] = React.useState<Date>(new Date());
 
   React.useEffect(() => {
     setLandList(getTerritoryNames(landType));
   }, [landType]);
 
   const handleLandTypeChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-    setLandType(event.target.value as TerritoryType);
+    setLandType(event.target.value as LandType);
+  };
+
+  const handlePlaceDateChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setSelectDate(event.target.value as Date);
   };
 
   const {
@@ -41,8 +46,28 @@ export const ClassroomCreate = () => {
     formState: { errors },
   } = useForm();
 
+  const onSubmit: SubmitHandler<Input> = (data, e) => {
+    e?.preventDefault();
+    console.log('login', data);
+
+    e?.target.reset();
+    reset();
+  };
+
+  type Input = {
+    idCal: string;
+    colaborator: string;
+    //place class 📌
+    place: { name: string; dir: string; date: Date };
+    //objective land 🗾🗺
+    landType: LandType;
+    landName: string;
+    //place delivery🚚
+    post: { name: string; dir: string; date: Date };
+  };
+
   return (
-    <Container maxWidth='xs'>
+    <Container maxWidth='sm'>
       <br />
       <Paper
         elevation={3}
@@ -50,7 +75,7 @@ export const ClassroomCreate = () => {
           padding: 15,
         }}
       >
-        <form autoComplete='off'>
+        <form autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
           <Grid
             container
             spacing={2}
@@ -60,45 +85,56 @@ export const ClassroomCreate = () => {
             wrap='wrap'
           >
             <Grid item xs={12}>
-              <Typography variant='h6' color='initial'>
+              <Typography variant='subtitle1' color='primary'>
                 nueva actividad
               </Typography>
             </Grid>
-            <Grid item xs={12}>
+            <Grid item xs={4}>
               <TextField
+                required
                 id='standard-required'
                 label='idCal'
                 type='number'
                 variant='outlined'
+                {...register('idCal', {
+                  max: { value: 1000, message: 'muy grande' },
+                  min: { value: 0, message: 'no negativo' },
+                })}
+                error={errors.idCal && true}
+                helperText={errors.idCal?.message}
               />
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={8}>
               {/*date picker 📆📅*/}
               <TextField
-                id='standard-required'
-                type='date'
+                id='datetime-local'
+                type='datetime-local'
+                label='fecha/hora'
                 variant='outlined'
+                color='primary'
+                InputLabelProps={{
+                  shrink: true,
+                }}
                 fullWidth
+                {...register('placeDate', {
+                  validate: { morThan: (v) => v > Date.now() },
+                })}
+                error={errors.placeDate && true}
+                onChange={handlePlaceDateChange}
+                helperText={errors.placeDate && true ? 'fecha pasada' : undefined}
               />
             </Grid>
-            <Grid item xs={6}>
-              {/*time picker 🕒🕔🕕*/}
-              <TextField
-                id='standard-required'
-                type='time'
-                variant='outlined'
-                fullWidth
-              />
-            </Grid>
+
             <Grid item xs={5}>
-              {/*lugar*/}
+              {/*lugar🔰📌*/}
               <TextField
                 required
                 id='standard-required'
                 type='text'
-                label='nombre lugar'
+                label='lugar'
                 variant='outlined'
                 fullWidth
+                {...register('placeName', {})}
               />
             </Grid>
             <Grid item xs={7}>
@@ -107,25 +143,28 @@ export const ClassroomCreate = () => {
                 required
                 id='standard-required'
                 type='text'
-                label='dirección o url'
+                label='dirección'
                 variant='outlined'
                 fullWidth
+                {...register('placeDir', {})}
               />
             </Grid>
 
             <Grid item xs={12}>
               <TextField
+                required
                 fullWidth
                 variant='outlined'
                 id='standard-required'
-                label='contraparte'
+                label='contraparte organizadora'
                 type='text'
                 inputProps={{ style: { textTransform: 'capitalize' } }}
               />
             </Grid>
+
             <Grid item xs={12}>
               <Typography variant='subtitle2' color='primary'>
-                Depliege territorial
+                Depliege territorial 🌐
               </Typography>
             </Grid>
 
@@ -138,9 +177,9 @@ export const ClassroomCreate = () => {
                   value={landType}
                   onChange={handleLandTypeChange}
                 >
-                  <MenuItem value={TerritoryType.city}>Comunal</MenuItem>
-                  <MenuItem value={TerritoryType.province}>Provincial</MenuItem>
-                  <MenuItem value={TerritoryType.region}>Regional</MenuItem>
+                  <MenuItem value={LandType.city}>Comunal</MenuItem>
+                  <MenuItem value={LandType.province}>Provincial</MenuItem>
+                  <MenuItem value={LandType.region}>Regional</MenuItem>
                 </Select>
               </FormControl>
             </Grid>
@@ -155,14 +194,65 @@ export const ClassroomCreate = () => {
                     label='territorio'
                     variant='filled'
                     required
-                    {...register('land', {})}
+                    {...register('landName', {})}
                     error={errors.land && true}
                     helperText={errors.land && true ? 'nombre requerido' : undefined}
                   />
                 )}
               />
             </Grid>
-            <Grid item xs={12}></Grid>
+
+            <Grid item xs={12}>
+              <Typography variant='subtitle2' color='primary'>
+                Punto de entrega 🚚
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              {/*date delivery Picker 📆📅*/}
+              <TextField
+                id='datetime-local'
+                type='datetime-local'
+                label='fecha/hora'
+                variant='outlined'
+                color='primary'
+                value={selectDate}
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                fullWidth
+                {...register('postDate', {
+                  validate: { morThan: (v) => v > Date.now() },
+                })}
+                error={errors.placeDate && true}
+                helperText={errors.placeDate && true ? 'fecha pasada' : undefined}
+              />
+            </Grid>
+
+            <Grid item xs={5}>
+              {/*lugar🔰📌*/}
+              <TextField
+                required
+                id='standard-required'
+                type='text'
+                label='lugar entrega'
+                variant='outlined'
+                fullWidth
+                {...register('postName', {})}
+              />
+            </Grid>
+            <Grid item xs={7}>
+              {/*address 🗺🦝🗾*/}
+              <TextField
+                required
+                id='standard-required'
+                type='text'
+                label='dirección'
+                variant='outlined'
+                fullWidth
+                {...register('postDir', {})}
+              />
+            </Grid>
+
             <Grid item xs={12}>
               <Button variant='contained' color='primary' fullWidth>
                 crear
