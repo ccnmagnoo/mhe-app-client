66300da8c95574165c338fcc26b9505c547623a1
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-06-28 00:09:31 -0400

    restricted dashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { Welcome } from './Components/Welcome';
 import { Container } from '@material-ui/core';
 import { Logo } from './Components/Logo';
 import { ButtonNav } from './Components/ButtonNav';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { Login } from './Components/Restricted/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Components/Restricted/Login';
+import Dashboard from './Components/Restricted/Dashboard';
 
 function App() {
   const [use, setUse] = React.useState(0);
   return (
     <div className='App'>
       <Router>
-        <Container maxWidth='xs'>
+        <br />
+        <Container maxWidth='sm'>
           <Logo size={200} name={'Con Buena Energía'} />
           <br />
           <ButtonNav />
@@ -19,6 +21,9 @@ function App() {
           <Switch>
             <Route path='/login' exact>
               <Login />
+            </Route>
+            <Route path='/dashboard' exact>
+              <Dashboard />
             </Route>
             <Route path='/' exact>
               <Welcome />
