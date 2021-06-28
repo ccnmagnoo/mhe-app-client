8cf9d9db9d7e9a908ece90faf7276e5678c50a00
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-06-28 18:22:04 -0400

    warning issue

--- src/Components/Public/ButtonNav.tsx
+++ src/Components/Public/ButtonNav.tsx
@@ -6,28 +6,26 @@ import { Link } from 'react-router-dom';
 export const ButtonNav = () => {
   return (
     <React.Fragment>
-      <Box alignContent>
-        <ButtonGroup
-          variant='contained'
-          color='primary'
-          aria-label='navegación'
-          fullWidth
-          size='large'
-        >
-          <Button component={Link} to='/suscription'>
-            Incribete
-          </Button>
-          <Button component={Link} to='/validation'>
-            Valida
-          </Button>
-          <Button component={Link} to='/help'>
-            Consultas
-          </Button>
-          <Button component={Link} to='/login'>
-            Admin
-          </Button>
-        </ButtonGroup>
-      </Box>
+      <ButtonGroup
+        variant='contained'
+        color='primary'
+        aria-label='navegación'
+        fullWidth
+        size='large'
+      >
+        <Button component={Link} to='/suscription'>
+          Incribete
+        </Button>
+        <Button component={Link} to='/validation'>
+          Valida
+        </Button>
+        <Button component={Link} to='/help'>
+          Consultas
+        </Button>
+        <Button component={Link} to='/login'>
+          Admin
+        </Button>
+      </ButtonGroup>
     </React.Fragment>
   );
 };
