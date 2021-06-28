96b69c164ab30c2aa84c6d658b4e136f687b060f
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-06-28 00:07:53 -0400

    adapter init

--- src/Components/Restricted/Classroom.Adapter.tsx
+++ src/Components/Restricted/Classroom.Adapter.tsx
@@ -1,5 +1,27 @@
+import { Card, Avatar, CardHeader, IconButton } from '@material-ui/core';
 import React from 'react';
+import { MoreVert as MoreVertIcon } from '@material-ui/icons';
+import moment from 'moment';
+import 'moment/locale/es';
 
 export const ClassroomAdapter = () => {
-  return <React.Fragment></React.Fragment>;
+  const classDate = new Date();
+  const myTime = moment(classDate).format('dddd DD MMMM YYYY');
+
+  return (
+    <React.Fragment>
+      <Card>
+        <CardHeader
+          avatar={<Avatar aria-label=''>R</Avatar>}
+          action={
+            <IconButton aria-label=''>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title='R295 Prodemu Quillota'
+          subheader={myTime}
+        />
+      </Card>
+    </React.Fragment>
+  );
 };
