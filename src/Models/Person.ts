ae027a4f5c818ee89b4f0bc45caf5dbd4fb97ed0
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-06-29 11:27:04 -0400

    rut chequer code

--- src/Models/Person.ts
+++ src/Models/Person.ts
@@ -42,4 +42,46 @@ export class Person implements IPerson {
       return Gender.male;
     }
   }
+
+  isRol(rol: string): boolean {
+    //social number verification
+    const rolChecker = (rol?: string): boolean => {
+      //undefinded solution
+      if (rol === undefined) {
+        return false;
+      }
+
+      rol = rol?.replace('‐', '-');
+      rol = rol?.split('.').join('');
+      const re: RegExp = /^[0-9]+[-|‐]{1}[0-9kK]{1}$/;
+
+      if (!re.test(rol)) {
+        return false;
+      } else {
+        const rolSplited = rol.split('-');
+        let rolDigit = rolSplited[1];
+        const rolBody = rolSplited[0];
+
+        if (rolDigit === 'K') {
+          rolDigit = 'k';
+        }
+
+        return rolDigitGen(+rolBody) === rolDigit;
+      }
+    };
+
+    const rolDigitGen = (rol: number): number | string => {
+      //generador de verificador de código calculado
+      let M = 0,
+        S = 1;
+
+      for (; rol; rol = Math.floor(rol / 10)) {
+        S = (S + (rol % 10) * (9 - (M++ % 6))) % 11;
+      }
+
+      return S ? S - 1 : 'k';
+    };
+
+    return rolChecker(rol);
+  }
 }
