1c8dbbffa6af6f4b838e56ea1ad988a36aae132b
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-07-02 13:56:02 -0400

    lint fix

--- src/Functions/IsURL.ts
+++ src/Functions/IsURL.ts
@@ -1,6 +1,7 @@
 export const isUrl = (input: string): boolean => {
   const urlExpresion: RegExp =
-    /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+    /[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
+  //    /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
 
   return urlExpresion.test(input);
 };
