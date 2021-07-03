19bc5233fe84ca5f097274d1db18c15da0e2d6df
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-07-03 11:17:35 -0400

    capitalize //First Letters Uppercase

---
+++ src/Functions/capitalize.ts
@@ -0,0 +1,8 @@
+export function capitalize(s: string): string {
+  const list = s.split(' ');
+  const res = list.map((w) => {
+    return w.charAt(0).toUpperCase() + w.slice(1);
+  });
+
+  return res.join(' ');
+}
