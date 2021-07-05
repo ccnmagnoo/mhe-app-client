d176f83cd33505f2f481d9f4cfc04412dbdcf16b
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-07-05 15:59:50 -0400

    upload people

--- src/Components/Public/Help.tsx
+++ src/Components/Public/Help.tsx
@@ -22,6 +22,8 @@ import cvn26000 from '../../Config/mhe-data-benefit-26000.json';
 import cvn28000 from '../../Config/mhe-data-benefit-28000.json';
 import { refUuid } from '../../Config/credential';
 import { db } from '../../Config/firebase';
+import { IBeneficiary } from '../../Models/Beneficiary.interface';
+import { Gender } from '../../Models/Person.Interface';
 
 type RoomJson = {
   city: string;
@@ -127,7 +129,113 @@ export const Help = () => {
     console.countReset('data room to upload');
   }
 
-  function uploadPeople() {}
+  function uploadPeople() {
+    const physicalSign = `<svg
+      width="396.5"
+      height="200"
+      version="1.1"
+      id="svg6"
+      sodipodi:docname="signsvg.svg"
+      inkscape:version="1.1 (c68e22c387, 2021-05-23)"
+      xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+      xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:svg="http://www.w3.org/2000/svg">
+     <defs
+        id="defs10" />
+     <sodipodi:namedview
+        id="namedview8"
+        pagecolor="#ffffff"
+        bordercolor="#666666"
+        borderopacity="1.0"
+        inkscape:pageshadow="2"
+        inkscape:pageopacity="0.0"
+        inkscape:pagecheckerboard="0"
+        showgrid="false"
+        inkscape:snap-text-baseline="true"
+        inkscape:zoom="1.7994956"
+        inkscape:cx="80.855992"
+        inkscape:cy="143.09566"
+        inkscape:window-width="1920"
+        inkscape:window-height="986"
+        inkscape:window-x="-11"
+        inkscape:window-y="-11"
+        inkscape:window-maximized="1"
+        inkscape:current-layer="svg6" />
+     <text
+        xml:space="preserve"
+        style="font-size:14.6667px;line-height:1.25;font-family:'Arial Rounded MT Bold';-inkscape-font-specification:'Arial Rounded MT Bold, ';fill:#b4b4d3;stroke:none;stroke-opacity:1;fill-opacity:1"
+        x="179.42964"
+        y="96.082672"
+        id="text3151"><tspan
+          sodipodi:role="line"
+          id="tspan3149"
+          x="179.42964"
+          y="96.082672"
+          style="font-size:14.6667px;fill:#b4b4d3;stroke:none;stroke-opacity:1;fill-opacity:1">firma</tspan><tspan
+          sodipodi:role="line"
+          x="179.42964"
+          y="114.41605"
+          id="tspan3153"
+          style="font-size:14.6667px;fill:#b4b4d3;stroke:none;stroke-opacity:1;fill-opacity:1">fisica</tspan></text>
+     <rect
+        style="fill:none;fill-opacity:1;fill-rule:evenodd;stroke:#d1d2e4;stroke-width:7.29406;stroke-linecap:square;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;paint-order:stroke markers fill"
+        id="rect13367"
+        width="77.156387"
+        height="63.819317"
+        x="159.67181"
+        y="68.09034" />
+   </svg>
+   `;
+    console.log('upload people click');
+    console.log('size of consolidated people list', cvn.length);
+
+    const uploadPeopleToFirebase = async () => {
+      try {
+        //for each item on json file repository
+        cvn.forEach((list) => {
+          list.forEach((person) => {
+            //build <IBeneficiary>
+            console.log('preparin to upload: ', person.rut);
+            const itDate = new Date(person.dateBenefit);
+            const itGender = person.gender as Gender;
+            const beneficiary: IBeneficiary = {
+              uuid: person.puuid,
+              name: {
+                firstName: person.firstName,
+                fatherName: person.fatherName,
+                motherName: person.motherName,
+              },
+              rut: person.rut,
+              classroom: {
+                idCal: person.idCal,
+                uuid: person.uuidRoom,
+                dateInstance: itDate,
+              },
+              gender: itGender,
+              dateUpdate: itDate,
+              email: 'no-data',
+              phone: 'no-data',
+              address: { dir: person.dir, city: person.city },
+              sign: physicalSign,
+              dateSign: itDate,
+            };
+            //upload to firebase
+            const ref = db
+              .collection(`Activity/${refUuid}/Consolidated`)
+              .doc(beneficiary.uuid);
+            ref.set(beneficiary);
+            console.count('success on person');
+          });
+        });
+      } catch (error) {
+        console.log('fail uploading person', error);
+      }
+    };
+
+    uploadPeopleToFirebase();
+    console.countReset('success on person');
+  }
 
   return (
     <React.Fragment>
