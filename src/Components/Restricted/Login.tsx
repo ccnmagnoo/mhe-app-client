b0117ef9390e13434e9ac8f27ccf1ed3098a5c57
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-07-05 19:01:57 -0400

    grow transition

--- src/Components/Restricted/Login.tsx
+++ src/Components/Restricted/Login.tsx
@@ -6,6 +6,7 @@ import {
   Button,
   Box,
   Container,
+  Grow,
 } from '@material-ui/core';
 import React from 'react';
 import { withRouter } from 'react-router-dom';
@@ -61,66 +62,68 @@ const Login = (props: any) => {
   return (
     <React.Fragment>
       <Container maxWidth='xs'>
-        <Card>
-          <Box p={2}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Grid
-                container
-                spacing={1}
-                direction='column'
-                justify='center'
-                alignItems='center'
-                alignContent='center'
-                wrap='nowrap'
-              >
-                <Grid item xs={12}>
-                  <Typography variant='h6' color='primary'>
-                    acceso admin
-                  </Typography>
-                </Grid>
+        <Grow in={true}>
+          <Card>
+            <Box p={2}>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <Grid
+                  container
+                  spacing={1}
+                  direction='column'
+                  justify='center'
+                  alignItems='center'
+                  alignContent='center'
+                  wrap='nowrap'
+                >
+                  <Grid item xs={12}>
+                    <Typography variant='h6' color='primary'>
+                      acceso admin
+                    </Typography>
+                  </Grid>
 
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    id='email'
-                    type='email'
-                    label='email'
-                    variant='filled'
-                    {...register('email', {
-                      pattern: {
-                        value: /\b[\w.-]+@[\w.-]+\.\w{2,4}\b/,
-                        message: 'email inválido',
-                      },
-                    })}
-                    error={errors.email && true}
-                    helperText={errors.email?.message}
-                  />
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      id='email'
+                      type='email'
+                      label='email'
+                      variant='filled'
+                      {...register('email', {
+                        pattern: {
+                          value: /\b[\w.-]+@[\w.-]+\.\w{2,4}\b/,
+                          message: 'email inválido',
+                        },
+                      })}
+                      error={errors.email && true}
+                      helperText={errors.email?.message}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      id='password'
+                      type='password'
+                      label='password'
+                      variant='filled'
+                      {...register('password')}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button variant='contained' color='primary' type='submit'>
+                      Ingresar
+                    </Button>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography variant='caption' color='initial'>
+                      {error}
+                      {error !== null ? '🔔' : ''}
+                    </Typography>
+                  </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    required
-                    id='password'
-                    type='password'
-                    label='password'
-                    variant='filled'
-                    {...register('password')}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button variant='contained' color='primary' type='submit'>
-                    Ingresar
-                  </Button>
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography variant='caption' color='initial'>
-                    {error}
-                    {error !== null ? '🔔' : ''}
-                  </Typography>
-                </Grid>
-              </Grid>
-            </form>
-          </Box>
-        </Card>
+              </form>
+            </Box>
+          </Card>
+        </Grow>
       </Container>
     </React.Fragment>
   );
