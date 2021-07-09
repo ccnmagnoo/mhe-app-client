10d2df29b904ce47bd6dc7caf42259e3d94c41aa
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-07-09 14:23:51 -0400

    adapter beneficiarios

--- src/Components/Restricted/Adapter/roomAccordion.tsx
+++ src/Components/Restricted/Adapter/roomAccordion.tsx
@@ -6,6 +6,9 @@ import {
   Avatar,
   Typography,
   AccordionDetails,
+  Badge,
+  Button,
+  ButtonGroup,
 } from '@material-ui/core';
 import moment from 'moment';
 import React from 'react';
@@ -13,17 +16,47 @@ import { IClassroom } from '../../../Models/Classroom.interface';
 
 //icons
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
+import VisibilityIcon from '@material-ui/icons/Visibility';
 
-export const RoomAccordion = (props: {
+import { UrlChip } from '../../Public/UrlChip';
+import { refUuid } from '../../../Config/credential';
+import { dbKey } from '../../../Models/databaseKeys';
+import { db } from '../../../Config/firebase';
+import { IPerson, iPersonConverter } from '../../../Models/Person.Interface';
+
+const RoomAccordion = (props: {
   room: IClassroom;
   expanded: string | boolean;
   handleAccordionChange: (
     panel: string
   ) => (event: React.ChangeEvent<{}>, isExpanded: boolean) => void;
 }) => {
+  //input props
   const room = props.room;
   const expanded = props.expanded;
   const handleAccordionChange = props.handleAccordionChange;
+
+  //states
+  const [enrolled, setEnrolled] = React.useState<IPerson[]>([]);
+
+  //call beneficiaries/suscribed
+  const onSubmitBeneficiaries = async () => {
+    //call firebase suscribed
+    try {
+      const ref = db
+        .collection(`${dbKey.act}/${refUuid}/${dbKey.sus}`)
+        .withConverter(iPersonConverter);
+      const promises = room.enrolled.map((uuid) => {
+        return ref.doc(uuid).get();
+      });
+
+      const snapshot = await Promise.all(promises);
+      console.log('snapshots', snapshot.length);
+    } catch (error) {
+      console.log('error fetching suscribed', error);
+    }
+  };
 
   return (
     <>
@@ -36,26 +69,66 @@ export const RoomAccordion = (props: {
           aria-controls='panel1bh-content'
           id='panel1bh-header'
         >
-          <Grid container spacing={2} alignItems='center'>
-            <Grid item xs={3}>
+          <Grid container spacing={2} alignItems='center' justify='space-evenly'>
+            <Grid item xs={6} sm={2}>
               <Chip
                 avatar={<Avatar>R</Avatar>}
                 label={room.idCal.slice(1)}
                 color='secondary'
               />
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={6} sm={3}>
+              <Grid item xs={12}>
+                <Typography variant='caption' color='initial'>
+                  {moment(room.placeActivity.date).format('DD [de] MMM h:mm a')}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant='caption' color='primary'>
+                  {moment(room.placeActivity.date).endOf('day').fromNow()}
+                </Typography>
+              </Grid>
+            </Grid>
+
+            <Grid item sm={4} xs={6}>
               <Typography variant='caption' color='initial'>
-                {moment(room.placeActivity.date).format('DD [de] MMMM YY h:mm a')}
+                {room.colaborator}
               </Typography>
+            </Grid>
+
+            <Grid item sm={1} xs={3}>
+              <Badge badgeContent={room.enrolled.length} color='secondary'>
+                <GroupAddIcon color='primary' titleAccess={'inscritos'} />
+              </Badge>
             </Grid>
           </Grid>
         </AccordionSummary>
 
         <AccordionDetails>
-          <Typography>room content</Typography>
+          <Grid container spacing={1} justify='space-between' alignContent='center'>
+            <Grid item xs={6}>
+              <UrlChip url={room.placeActivity.dir} />
+            </Grid>
+
+            <Grid item xs={6} alignContent='flex-end'>
+              <ButtonGroup
+                variant='text'
+                color='primary'
+                aria-label='actividades-view'
+                size='small'
+              >
+                <Button onClick={onSubmitBeneficiaries}>
+                  <VisibilityIcon />
+                </Button>
+                <Button>editar</Button>
+                <Button>borrar</Button>
+              </ButtonGroup>
+            </Grid>
+          </Grid>
         </AccordionDetails>
       </Accordion>
     </>
   );
 };
+
+export { RoomAccordion };
