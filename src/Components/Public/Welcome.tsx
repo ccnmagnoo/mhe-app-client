3f6912d78d8a5a749ec85a31f135aa27cc6e6bd9
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-08-16 17:20:30 -0400

    link inside text

--- src/Components/Public/Welcome.tsx
+++ src/Components/Public/Welcome.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import { Card, CardContent, Grid, Avatar, CardHeader, Chip } from '@material-ui/core';
+import {
+  Card,
+  CardContent,
+  Grid,
+  Avatar,
+  CardHeader,
+  Chip,
+  Link,
+} from '@material-ui/core';
 import popipo from '../../Assets/popiposoft.svg';
 import EmailIcon from '@material-ui/icons/Email';
+import { NavLink } from 'react-router-dom';
 
 export const Welcome = () => {
   const mailTo =
@@ -23,13 +32,16 @@ export const Welcome = () => {
           <Grid container spacing={3} justify='center'>
             <Grid item>
               <Typography variant='subtitle1' color='inherit' align='justify' paragraph>
-                con esta mini app podrás suscribirte a los talleres y posteriormente
-                podrás validar tu asistencia a los Talleres de capacitación de{' '}
-                <strong>Seremi Energía Valparaíso</strong> de forma{' '}
+                con esta mini app podrás{' '}
+                <Link component={NavLink} to='/suscription' color='primary'>
+                  <strong>inscribirte a los talleres</strong>
+                </Link>{' '}
+                y posteriormente podrás validar tu asistencia a los Talleres de
+                capacitación de <strong>Seremi Energía Valparaíso</strong> de forma{' '}
                 <strong>on-line</strong>.
               </Typography>
               <Typography variant='caption' color='initial' paragraph>
-                Si tienes dudas escribenos a
+                Sólo consultas escribenos a{' '}
                 <Chip
                   avatar={<EmailIcon />}
                   label='@EnergíaValpo'
