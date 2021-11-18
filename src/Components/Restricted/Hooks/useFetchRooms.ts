f9c2c01c88deb5db15e4f1a9dfc5ab61ce93102d
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-11-18 17:42:44 -0300

    on snapshot listener implementation

--- src/Components/Restricted/Hooks/useFetchRooms.ts
+++ src/Components/Restricted/Hooks/useFetchRooms.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 import { refUuid } from '../../../Config/credential';
 import { db } from '../../../Config/firebase';
-import { iClassroomConverter } from '../../../Models/Classroom.interface';
+import { IClassroom, iClassroomConverter } from '../../../Models/Classroom.interface';
 import { dbKey } from '../../../Models/databaseKeys';
 import { Context } from '../Context/context';
 import { ActionType } from '../Context/reducer';
@@ -30,13 +30,33 @@ export const useFetchRooms = () => {
       .orderBy('placeActivity.date', 'desc')
       .withConverter(iClassroomConverter);
 
-    const snapshot = await ref.get();
-    const rooms = snapshot.docs.map((query) => {
-      return query.data();
+    //const snapshot = await ref.get();
+    const listOfRooms: IClassroom[] = [];
+
+    ref.onSnapshot((snapshot) => {
+      snapshot.docChanges().forEach((change, index, list) => {
+        switch (change.type) {
+          case 'added': {
+            const data = change.doc.data();
+            return (listOfRooms[index] = data);
+          }
+
+          case 'modified': {
+            const data = change.doc.data();
+            return listOfRooms.splice(index, 1, data);
+          }
+          case 'removed': {
+            return listOfRooms.splice(index, 1);
+          }
+
+          default:
+            return undefined;
+        }
+      });
+
+      //call Reducer
+      context.changeState({ type: ActionType.setRooms, payload: listOfRooms });
     });
-    //.sort((a, b) => (a.placeActivity.date > b.placeActivity.date ? -1 : 1))
-    console.log('useFetchRooms() got this rooms', rooms.length);
-    context.changeState({ type: ActionType.setRooms, payload: rooms });
   }
 
   return null;
