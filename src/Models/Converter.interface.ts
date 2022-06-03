a183f43620f41233fc854c34f4b82a47a118bf32
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-06-03 11:39:53 -0400

    converter interface

--- src/Models/Converter.interface.ts
+++ src/Models/Converter.interface.ts
@@ -1,6 +1,6 @@
 import { QueryDocumentSnapshot } from 'firebase/firestore';
 
 export default interface Converter<T> {
-  toFirestore([key:string]: T): T;
+  toFirestore(val: T): T;
   fromFirestore(snapshot: QueryDocumentSnapshot): T;
 }
