945b27fa370219487b1658a4fd0904355091cb7c
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-06-04 12:49:55 -0400

    region id getter

--- src/Functions/GetTerritoryList.ts
+++ src/Functions/GetTerritoryList.ts
@@ -1,21 +1,36 @@
 import { cities, City } from '../Assets/cities';
 
+/**
+ * @function getCityList function return all the cities in the territory requersted
+ * @param territory 's name  of Region, Province o Town,
+ * @param landType enum with city/province/region literals
+ */
 export function getCityList(territory: string, landType: LandType): string[] {
-  /**
-   * this function return all the cities in the territory requersted
-   */
-  const filterCities: City[] = cities.filter((land) => {
-    const cityObject: { [key: string]: string } = { ...land };
-    return cityObject[landType] === territory;
-  });
+  const filterCities = cities
+    .filter((land) => land[landType] === territory)
+    .map((land) => land.city);
 
-  const result = filterCities.map((land) => {
-    return land.city;
-  });
-
-  return result;
+  return filterCities;
 }
 
+/**
+ * @returns objecte @type City with territorial identifiers
+ * @param territory name
+ * @param landType literal of: region, province, city
+ */
+export function getTerritoryId(
+  territory: string,
+  landType: LandType
+): number | undefined {
+  const list = cities.find((land) => land[landType] === territory);
+  const id = list?.regionId;
+  return id ? +id : undefined;
+}
+
+/**
+ * @function getTerritoryNames, @return string list of names land
+ * @param landType , literal region,city,province
+ */
 export function getTerritoryNames(landType: LandType): string[] {
   /**
    * this function return all the names of specifict land type
