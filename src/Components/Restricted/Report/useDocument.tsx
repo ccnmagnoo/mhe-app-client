2fcb3dfbc5f506808289fe469059df729b6c3e9f
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-07-04 17:35:40 -0400

    react pdf blob

--- src/Components/Restricted/Report/useDocument.tsx
+++ src/Components/Restricted/Report/useDocument.tsx
@@ -2,18 +2,27 @@ import { Certificate } from './Certificate';
 import { IBeneficiary } from '../../../Models/Beneficiary.interface';
 import { IRoom } from '../../../Models/Classroom.interface';
 import { Document, pdf } from '@react-pdf/renderer';
+import { useEffect, useState } from 'react';
 
-export async function useDocument(room: IRoom, people: IBeneficiary[]) {
-  const blobList = people.map((p, i) => {
-    const blob = (
-      <Document>
-        <Certificate person={p} room={room} index={i} key={i} />
-      </Document>
-    );
-    return pdf(blob).toBlob();
-  });
+export function useDocument(room: IRoom, people: IBeneficiary[]) {
+  const [blob, setBlob] = useState<Blob[] | undefined>(undefined);
+  useEffect(() => {
+    const blobList = people.map((p, i) => {
+      const blob = (
+        <Document>
+          <Certificate person={p} room={room} index={i} key={i} />
+        </Document>
+      );
+      return pdf(blob).toBlob();
+    });
 
-  const document = await Promise.all(blobList);
+    const document = async () => {
+      const fetch = await Promise.all(blobList);
+      setBlob(fetch);
+    };
 
-  return document;
+    document();
+  }, [people, room]);
+
+  return blob;
 }
