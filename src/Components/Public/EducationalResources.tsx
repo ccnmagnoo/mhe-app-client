ee92c8ee26ac0d9c8e15cf4b30a7655836e3f21c
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-12-26 10:47:29 -0300

    educational page layout

--- src/Components/Public/EducationalResources.tsx
+++ src/Components/Public/EducationalResources.tsx
@@ -3,21 +3,21 @@ import Typography from '@material-ui/core/Typography';
 
 export const EducationalResources = () => {
   return (
-    <div>
+    <>
       <header>
         <Typography variant='h5' color='primary'>
-          Expora & aprende
+          Explora & aprende
         </Typography>
 
         <Typography variant='body1' color='textPrimary'>
           Porque cada día se puede aprender algo más
         </Typography>
       </header>
-      <body>
+      <main>
         <section>ppt canva</section>
         <section>video carrousel</section>
         <section>apps</section>
-      </body>
-    </div>
+      </main>
+    </>
   );
 };
