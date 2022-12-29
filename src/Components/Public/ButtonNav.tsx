a391390323c6399ff86ee7049d0013d3a1d39411
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-12-29 15:17:52 -0300

    reactive button nav

--- src/Components/Public/ButtonNav.tsx
+++ src/Components/Public/ButtonNav.tsx
@@ -6,36 +6,35 @@ import FaceIcon from '@material-ui/icons/Face';
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import LockIcon from '@material-ui/icons/Lock';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import './ButtonNav.css';
 
 import { NavLink } from 'react-router-dom';
 
 export const ButtonNav = () => {
   return (
-    <>
+    <section id='main-navigation'>
       <ButtonGroup
         variant='contained'
         color='primary'
         aria-label='navegación'
         fullWidth
-        size='large'
+        size='medium'
       >
         <Button component={NavLink} to='/suscription' startIcon={<FaceIcon />}>
-          Incribete
+          <span>Inscribete</span>
         </Button>
         <Button component={NavLink} to='/validation' startIcon={<VerifiedUserIcon />}>
-          Valida
+          <span>Valida</span>
         </Button>
 
         <Button component={NavLink} to='/resources' startIcon={<PlayCircleOutlineIcon />}>
-          Aprende
+          <span>Aprende</span>
         </Button>
-        {/*<Button component={NavLink} to='/help'>
-          Consultas
-        </Button>*/}
+
         <Button component={NavLink} to='/login' startIcon={<LockIcon />}>
-          Admin
+          <span>Admin</span>
         </Button>
       </ButtonGroup>
-    </>
+    </section>
   );
 };
