179b46261e37855dc0a755008756a0558c3a640a
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2022-12-29 21:12:21 -0300

    mobile app item

--- src/Components/Public/Educational/Mobile.tsx
+++ src/Components/Public/Educational/Mobile.tsx
@@ -1,4 +1,8 @@
 import Typography from '@material-ui/core/Typography';
+import os_android from '../../../Assets/os_android.svg';
+import os_apple from '../../../Assets/os_apple.svg';
+import os_desktop from '../../../Assets/os_desktop.svg';
+import './Mobile.css';
 
 type MobileOS = {
   os: 'android' | 'apple' | 'desktop';
@@ -28,15 +32,82 @@ class MobileApp implements MobileProps {
 
   component(): JSX.Element {
     return (
-      <article className='Mobile-app-item'>
+      <article className='mobile-app-item'>
+        <div className='mobile-side-panel'>
+          {/* side panel by default opacity 0 */}
+          <h5>{this.name}</h5>
+          <p>
+            {this.description} <span>.</span>{' '}
+          </p>
+          {this.buildButtons()}
+        </div>
         <img src={this.logo} alt={this.name} width={100} height={100} />
+        <div className='mobile-over-panel'>{this.buildButtons()}</div>
       </article>
+    );
+  }
+
+  private getOsLogo(os: 'android' | 'apple' | 'desktop') {
+    switch (os) {
+      case (os = 'android'):
+        return <img src={os_android} alt={os} />;
+      case (os = 'apple'):
+        return <img src={os_apple} alt={os} />;
+      case (os = 'desktop'):
+        return <img src={os_desktop} alt={os} />;
+      default:
+        return undefined;
+    }
+  }
+  private buildButtons() {
+    return (
+      <div className='os-selector-container'>
+        {this.platform.map((osVersion) => {
+          return (
+            <a href={osVersion.link} target='_blank' rel='noreferrer'>
+              <div className='os-button'>{this.getOsLogo(osVersion.os)}</div>
+            </a>
+          );
+        })}
+      </div>
     );
   }
 }
 
 const Mobile = () => {
   const appList: MobileApp[] = [
+    new MobileApp({
+      name: 'Explora Tu Energía',
+      logo: 'https://play-lh.googleusercontent.com/q5py2ne3MfyW9DgXBx70E3c8Zev9ELfaBp3tBLF_X0f4iuZaD5nqmVw9T_5FpBRWog=w240-h480-rw',
+      description: 'explora con realidad virtual de donde viene tu energía',
+      size: 100,
+      platform: [
+        {
+          os: 'android',
+          link: 'https://play.google.com/store/apps/details?id=cl.minenergia.exploratuenergia&hl=es_419&gl=US',
+        },
+        {
+          os: 'apple',
+          link: 'https://apps.apple.com/cl/app/explora-tu-energ%C3%ADa/id1494385419',
+        },
+      ],
+    }),
+    new MobileApp({
+      name: 'Explora Tu Energía',
+      logo: 'https://play-lh.googleusercontent.com/q5py2ne3MfyW9DgXBx70E3c8Zev9ELfaBp3tBLF_X0f4iuZaD5nqmVw9T_5FpBRWog=w240-h480-rw',
+      description: 'explora con realidad virtual de donde viene tu energía',
+      size: 100,
+      platform: [
+        {
+          os: 'android',
+          link: 'https://play.google.com/store/apps/details?id=cl.minenergia.exploratuenergia&hl=es_419&gl=US',
+        },
+        {
+          os: 'apple',
+          link: 'https://apps.apple.com/cl/app/explora-tu-energ%C3%ADa/id1494385419',
+        },
+      ],
+    }),
     new MobileApp({
       name: 'Explora Tu Energía',
       logo: 'https://play-lh.googleusercontent.com/q5py2ne3MfyW9DgXBx70E3c8Zev9ELfaBp3tBLF_X0f4iuZaD5nqmVw9T_5FpBRWog=w240-h480-rw',
